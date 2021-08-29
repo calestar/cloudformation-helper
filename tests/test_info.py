fae1907a73db04b0a6ca0fca90bffc62cf81b8ad
Jean-Sébastien Gélinas <calestar@gmail.com>
2021-08-29 01:28:46 -0400

    Make test_info a bit better
    Signed-off-by: Jean-Sébastien Gélinas <calestar@gmail.com>

--- tests/test_info.py
+++ tests/test_info.py
@@ -4,6 +4,7 @@
 
 from click.testing import CliRunner
 
+import cloudformation_helper
 from cloudformation_helper import cli
 
 
@@ -11,10 +12,15 @@ def test_info():
     """Not much to test, simply check result from the call"""
     runner = CliRunner()
 
-    runner.invoke(
+    result = runner.invoke(
         cli.cfhelper,
         [
             "info",
         ],
         catch_exceptions=False,
+    )
+
+    assert result.exit_code == 0
+    assert (
+        f"cloudformation-helper: {cloudformation_helper.__version__}" in result.output
     )
